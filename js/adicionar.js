ad05acfdf0561399eec0b3227accaa1705a62649
jhonatan.araujo <jhonatan.araujo@grupoboticario.com.br>
2023-10-27 00:58:48 -0300

    adicionado melhorias e comentarios.

--- js/adicionar.js
+++ js/adicionar.js
@@ -5,34 +5,61 @@ btnAdicionar.addEventListener("click", adicionarRequisicao)
 
 // Função para adicionar uma nova requisição à lista
 function adicionarRequisicao() {
-  const disciplinaInput = document.getElementById('disciplina');
-  const descricaoInput = document.getElementById('descricao');
-  const dataInput = document.getElementById('dataDeConclusao');
-  const feitoInput = false; // Defina o valor padrão como "Não feito"
-  const progressoInput = "olá"; // Defina o valor padrão como vazio
+    const disciplinaInput = document.getElementById('disciplina');
+    const descricaoInput = document.getElementById('descricao');
+    const dataInput = document.getElementById('dataDeConclusao');
+    const feitoInput = false; // Defina o valor padrão como "Não feito"
+    const progressoInput = ""; // Defina o valor padrão como vazio
 
 
-  const novoItem = {
-    "disciplina": disciplinaInput.value,
-    "descricao": descricaoInput.value,
-    "dataDeConclusao": dataInput.value,
-    "feito": feitoInput,
-    "progresso": progressoInput
-  };
+    const novoItem = {
+        "disciplina": disciplinaInput.value,
+        "descricao": descricaoInput.value,
+        "dataDeConclusao": dataInput.value,
+        "feito": feitoInput,
+        "progresso": progressoInput
+    };
 
-  listaJSON.push(novoItem);
+    //Adicionado a funcao de salvar no LOCALSTORAGE
+     salvarDadosNoLocalStorage(novoItem);
 
-  // Limpa os campos do formulário
-  disciplinaInput.value = '';
-  descricaoInput.value = '';
-  dataInput.value = '';
-  feitoInput.value = '';
+    // Limpa os campos do formulário
+    disciplinaInput.value = '';
+    descricaoInput.value = '';
+    dataInput.value = '';
+    feitoInput.value = '';
 
+    // Atualiza a lista de requisições na tabela da interface
+    atualizarListaRequisicoes();
 
-  // Atualiza a lista de requisições na tabela da interface
-  atualizarListaRequisicoes();
+    console.table(buscarListaLocalStorage); // Exibe a lista JSON atualizada no console
+}
 
-  // Salva os dados no Local Storage
-  salvarDadosNoLocalStorage();
+//Este metodo esta salvando um item no localStorage
+function salvarDadosNoLocalStorage(novoItem) {
+    //buscamos a listagem que esta no formato de string
+    let storedData = localStorage.getItem('listaJSON');
+    // Iniciamos uma variavel onde iremos transforma a lista armazenada
+    // em string em uma lista de objeto
+    let transformandoDeStringParaListagem = [];
 
-  }
+    //Verificamos se contem conteudo dentro do localstorage
+    if (storedData) {
+        // Se há dados no localStorage,
+        // faça o parsing dos dados existentes transformando a string em um array de objeto
+        transformandoDeStringParaListagem = JSON.parse(storedData);
+
+        //verificamos se a o localstorage retornou 1 array ou somente 1 item
+        if (!Array.isArray(transformandoDeStringParaListagem)) {
+            // Se não for um array, criamos um array com o item do localstorage
+            transformandoDeStringParaListagem = [transformandoDeStringParaListagem];
+        }
+    }
+    // inserimos o novo item dentro do array
+    transformandoDeStringParaListagem.push(novoItem)
+
+// Salve a lista atualizada de volta no localStorage,
+// sendo assim sempre mantendo o localstorage atualizado para a busca
+    localStorage.setItem('listaJSON', JSON.stringify(transformandoDeStringParaListagem));
+    return novoItem
+}
--- js/localStorage.js
+++ js/localStorage.js
@@ -1,22 +1,18 @@
 // Função para carregar os dados da lista a partir do Local Storage
 function carregarDadosDoLocalStorage() {
-  const dadosLocalStorage = localStorage.getItem('listaJSON');
-  if (dadosLocalStorage) {
-    listaJSON = JSON.parse(dadosLocalStorage);
-    atualizarListaRequisicoes();
+    const dadosLocalStorage = localStorage.getItem('listaJSON');
+    if (dadosLocalStorage) {
+      listaJSON = JSON.parse(dadosLocalStorage);
+      atualizarListaRequisicoes();
+    }
   }
-}
 
-// Função para salvar os dados da lista no Local Storage
-function salvarDadosNoLocalStorage() {
-  localStorage.setItem('listaJSON', JSON.stringify(listaJSON));
-}
+  // Função para salvar os dados da lista no Local Storage
+  function salvarDadosNoLocalStorage() {
+    localStorage.setItem('listaJSON', JSON.stringify(listaJSON));
+  }
+// Removido variavel let listaJSON = []; ja tinha sido instanciado outras vezes
 
-// Lista JSON existente
-let listaJSON = [];
-
-// Carregar dados do Local Storage, se existirem
-carregarDadosDoLocalStorage();
-
-// Inicializa a lista de requisições na tabela da interface
-atualizarListaRequisicoes();
+// Estas funcoes estao na pagina forEach,
+// depois voce organiza o código da maneira que achar melhor
+// removido   carregarDadosDoLocalStorage();
